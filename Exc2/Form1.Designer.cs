a2dbcee68debba63f400188199a19bb713fd294a
Joelson Botelho <silvabotelho20@gmail.com>
2023-11-07 18:17:10 +0100

    Initial commit

---
+++ Exc2/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Exc2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte do Designer - não modifique
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl = new DevComponents.DotNetBar.LabelX();
+            this.txtNum = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtNumRomana = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.btnRomana = new DevComponents.DotNetBar.ButtonX();
+            this.btnGuardar = new DevComponents.DotNetBar.ButtonX();
+            this.btnSair = new DevComponents.DotNetBar.ButtonX();
+            this.SuspendLayout();
+            // 
+            // lbl
+            // 
+            // 
+            // 
+            // 
+            this.lbl.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.lbl.ForeColor = System.Drawing.Color.White;
+            this.lbl.Location = new System.Drawing.Point(12, 12);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(230, 53);
+            this.lbl.Symbol = "58138";
+            this.lbl.SymbolSet = DevComponents.DotNetBar.eSymbolSet.Material;
+            this.lbl.TabIndex = 0;
+            this.lbl.Text = " Insira Um Número";
+            // 
+            // txtNum
+            // 
+            // 
+            // 
+            // 
+            this.txtNum.Border.Class = "TextBoxBorder";
+            this.txtNum.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtNum.Location = new System.Drawing.Point(249, 24);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.PreventEnterBeep = true;
+            this.txtNum.Size = new System.Drawing.Size(335, 30);
+            this.txtNum.TabIndex = 0;
+            // 
+            // txtNumRomana
+            // 
+            // 
+            // 
+            // 
+            this.txtNumRomana.Border.Class = "TextBoxBorder";
+            this.txtNumRomana.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtNumRomana.Location = new System.Drawing.Point(12, 158);
+            this.txtNumRomana.Name = "txtNumRomana";
+            this.txtNumRomana.PreventEnterBeep = true;
+            this.txtNumRomana.ReadOnly = true;
+            this.txtNumRomana.Size = new System.Drawing.Size(572, 30);
+            this.txtNumRomana.TabIndex = 2;
+            // 
+            // btnRomana
+            // 
+            this.btnRomana.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnRomana.ColorTable = DevComponents.DotNetBar.eButtonColor.BlueOrb;
+            this.btnRomana.Location = new System.Drawing.Point(12, 85);
+            this.btnRomana.Name = "btnRomana";
+            this.btnRomana.Size = new System.Drawing.Size(134, 48);
+            this.btnRomana.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnRomana.Symbol = "";
+            this.btnRomana.TabIndex = 3;
+            this.btnRomana.Text = " Romana";
+            this.btnRomana.Click += new System.EventHandler(this.btnRomana_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnGuardar.ColorTable = DevComponents.DotNetBar.eButtonColor.BlueOrb;
+            this.btnGuardar.Location = new System.Drawing.Point(224, 85);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(134, 48);
+            this.btnGuardar.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnGuardar.Symbol = "";
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = " Guardar";
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnSair.ColorTable = DevComponents.DotNetBar.eButtonColor.BlueOrb;
+            this.btnSair.Location = new System.Drawing.Point(449, 85);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(134, 48);
+            this.btnSair.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnSair.Symbol = "";
+            this.btnSair.TabIndex = 5;
+            this.btnSair.Text = " Sair";
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(596, 200);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnRomana);
+            this.Controls.Add(this.txtNumRomana);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.lbl);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.LabelX lbl;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtNum;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtNumRomana;
+        private DevComponents.DotNetBar.ButtonX btnRomana;
+        private DevComponents.DotNetBar.ButtonX btnGuardar;
+        private DevComponents.DotNetBar.ButtonX btnSair;
+    }
+}
+
---
+++ Exc2/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Exc2
+{
+    public partial class Form1 : Form
+    {
+        string caminho = @"C:\Users\hp\Documents\Joelson Botelho\Exc2\Exc2.txt";
+        string[] Unidade = new string[10] { " ", "I", "II", "III", "IV", "V", "VI", "VII", "VIII","IX" };
+        string[] Dezena = new string[10] { " ", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+        string[] Centena = new string[100] { " ", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", " ", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", " ", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", " ", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", " ", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", " ", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", " ", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", " ", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", " ", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", " ", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        string[] Milesimo = new string[10] { " ", "M", "MM", "MMM", "IV_", "V_", "VI_", "VII_", "VIII_", "IX_" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRomana_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int Numero = int.Parse(txtNum.Text);
+                int unidade = Numero % 10;
+                int dezena = (Numero / 10) % 10;
+                int centena = (Numero / 100);
+                int milesimo = (Numero / 1000);
+                if (Numero >= 10000)
+                {
+                    MessageBox.Show("Digite um Número no Intervalo de 1 à 9999");
+                }
+                else
+                {
+                    if (Numero >= 1000)
+                    {
+                        txtNumRomana.Text = Milesimo[milesimo] + Centena[centena] + Dezena[dezena] + Unidade[unidade];
+                        txtNumRomana.Text = txtNumRomana.Text.Replace(" ", "");
+                    }
+                    else
+                    {
+                        if (Numero >= 100)
+                        {
+                            txtNumRomana.Text = Centena[centena] + Dezena[dezena] + Unidade[unidade];
+                        }
+                        else
+                        {
+                            if (Numero >= 10)
+                            {
+                                txtNumRomana.Text = Dezena[dezena] + Unidade[unidade];
+                            }
+                            else
+                            {
+                                if (Numero < 0)
+                                {
+                                    MessageBox.Show("Digite Um Número Positivo");
+                                }
+                                else
+                                {
+                                    txtNumRomana.Text = Unidade[unidade];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Digite um Número");
+            }
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+           if( MessageBox.Show("Tem A Certeza Que Deseja Sair ??","Sair",MessageBoxButtons.YesNo,MessageBoxIcon.Question)==DialogResult.Yes)
+            Application.Exit();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string Tudo = txtNum.Text + " = " + txtNumRomana.Text;
+            List<string> doc = new List<string>();
+            if (File.Exists(caminho))
+            {
+                StreamReader rd = new StreamReader(caminho);
+                while (!rd.EndOfStream)
+                {
+                    doc.Add(rd.ReadLine());
+                }
+                rd.Close();
+                foreach (string item in doc)
+                {
+                    if (item == Tudo)
+                    {
+                        MessageBox.Show("Este Dado já foi Gravado");
+                        return;
+                    }
+                }
+                StreamWriter wr = new StreamWriter(caminho, true);
+                wr.WriteLine(Tudo);
+                wr.Close();
+            }
+            else
+            {
+                MessageBox.Show("Crie um Ficheiro Com o Nome Exc2 e Formato .txt");
+            }
+        }
+    }
+}
